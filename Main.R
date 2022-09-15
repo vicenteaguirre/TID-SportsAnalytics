0befadb5d514f44ae80facf0fbfb0884d9f8678f
Vicente Aguirre <69131900+vicenteaguirre@users.noreply.github.com>
2022-09-14 21:00:37 -0300

    Creando repositorio
    Subiendo el main.R, con el código del análisis. Además, se sube el dataset utilizado.

---
+++ Main.R
@@ -0,0 +1,4 @@
+pacman::p_load(tidymodels, tidyverse, readr)
+set.seed(42)
+data <- read_delim(file = "/Users/vaguirre/Desktop/UAI/2022/2do/TID-SportsAnalytics/Volley-datasets/beachvolley_matches.csv", delim = ";")
+
